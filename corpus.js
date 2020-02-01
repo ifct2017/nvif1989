c67076d1cf64bf46d80a1fc1260efb130cc9e3e0
Subhajit Sahu <wolfram77@gmail.com>
2020-02-01 19:36:34 +0530

    corpus generated

---
+++ corpus.js
@@ -0,0 +1,14 @@
+const CORPUS = new Map([
+  ["Egg", {name:"Egg",group:"Animal proteins",bv:96,npu:96,per:3.8}],
+  ["Milk", {name:"Milk",group:"Animal proteins",bv:90,npu:85,per:2.8}],
+  ["Meat", {name:"Meat",group:"Animal proteins",bv:74,npu:76,per:3.2}],
+  ["Fish", {name:"Fish",group:"Animal proteins",bv:80,npu:74,per:3.5}],
+  ["Rice", {name:"Rice",group:"Cereals",bv:80,npu:77,per:1.7}],
+  ["Wheat", {name:"Wheat",group:"Cereals",bv:66,npu:61,per:1.3}],
+  ["Maize", {name:"Maize",group:"Cereals",bv:50,npu:48,per:1}],
+  ["Bengal gram", {name:"Bengal gram",group:"Pulses",bv:74,npu:61,per:1.1}],
+  ["Red gram", {name:"Red gram",group:"Pulses",bv:72,npu:54,per:1.7}],
+  ["Groundnut", {name:"Groundnut",group:"Oilseeds",bv:55,npu:null,per:null}],
+  ["Gingelly", {name:"Gingelly",group:"Oilseeds",bv:62,npu:null,per:null}],
+]);
+module.exports = CORPUS;
