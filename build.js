41c7f54743178614ec08f8f9baff4f5100cb5999
Subhajit Sahu <wolfram77@gmail.com>
2020-02-01 14:19:06 +0000

    grup fixed

--- build.js
+++ build.js
@@ -51,6 +51,7 @@ function valParse(val, code, dat, i) {
 function readAssetRow(row) {
   var cod = row.code.trim();
   var des = descriptions(cod)[0];
+  var grp = groups(cod)[0];
   var old = map.has(cod);
   var nam = row.name.trim();
   var sci = des.scie;
@@ -59,8 +60,8 @@ function readAssetRow(row) {
   dat.name[i] = old && dat.name[i].length>nam.length? dat.name[i]:nam;
   dat.scie[i] = sci;
   dat.lang[i] = (des||{desc: ''}).desc;
-  dat.grup[i] = groups(cod[0])[0].group;
-  dat.tags[i] = groups(cod[0])[0].tags.trim();
+  dat.grup[i] = grp.group;
+  dat.tags[i] = grp.tags.trim();
   for(var k in row) {
     if(BASE.includes(k)) continue;
     var val = row[k].trim(), kt = renames.get(k)||k;
@@ -122,7 +123,6 @@ async function build() {
   }, new Map());
   for(var file of fs.readdirSync('assets'))
     await readAsset(path.join('assets', file));
-  console.log(dat.fatce[0]);
   nullToZero(dat);
   sumAll(dat);
   dat = orderAll(dat);
