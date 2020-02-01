4f6265087e03eadd294ffc2f97fd5c65131413e0
Subhajit Sahu <wolfram77@gmail.com>
2020-02-01 14:42:15 +0000

    main script available

---
+++ index.js
@@ -0,0 +1,6 @@
+exports.codes = require('@nvif1989/codes');
+exports.columns = require('@nvif1989/columns');
+exports.compositions = require('@nvif1989/compositions');
+exports.descriptions = require('@nvif1989/descriptions');
+exports.groups = require('@nvif1989/groups');
+exports.languages = require('@nvif1989/languages');
