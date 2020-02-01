a7dd0c6d5c0c74313ed83eda76748370f1dfefe7
Subhajit Sahu <wolfram77@gmail.com>
2020-02-01 18:26:03 +0530

    corpus generated

---
+++ corpus.js
@@ -0,0 +1,15 @@
+const CORPUS = new Map([
+  ["Adult male (sedentary worker)", {group:"Adult male (sedentary worker)",cu:"1.0"}],
+  ["Adult male (moderate worker)", {group:"Adult male (moderate worker)",cu:"1.2"}],
+  ["Adult male (heavy worker)", {group:"Adult male (heavy worker)",cu:"1.6"}],
+  ["Adult female (sedentary worker)", {group:"Adult female (sedentary worker)",cu:"0.8"}],
+  ["Adult female (moderate worker)", {group:"Adult female (moderate worker)",cu:"0.9"}],
+  ["Adult female (heavy worker)", {group:"Adult female (heavy worker)",cu:"1.2"}],
+  ["Adolescents 12 to 21 years", {group:"Adolescents 12 to 21 years",cu:"1.0"}],
+  ["Children 9 to 12 years", {group:"Children 9 to 12 years",cu:"0.8"}],
+  ["Children 7 to 9 years", {group:"Children 7 to 9 years",cu:"0.7"}],
+  ["Children 5 to 7 years", {group:"Children 5 to 7 years",cu:"0 .6"}],
+  ["Children 3 to 5 years", {group:"Children 3 to 5 years",cu:"0.5"}],
+  ["Children 1 to 3 years", {group:"Children 1 to 3 years",cu:"0.4"}],
+]);
+module.exports = CORPUS;
