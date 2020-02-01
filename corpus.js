edc3c2900991e51b88858ed1bcef63d1081c0612
Subhajit Sahu <wolfram77@gmail.com>
2020-02-01 18:57:58 +0530

    fix children 5-7 row

--- corpus.js
+++ corpus.js
@@ -8,7 +8,7 @@ const CORPUS = new Map([
   ["Adolescents 12 to 21 years", {group:"Adolescents 12 to 21 years",cu:1}],
   ["Children 9 to 12 years", {group:"Children 9 to 12 years",cu:0.8}],
   ["Children 7 to 9 years", {group:"Children 7 to 9 years",cu:0.7}],
-  ["Children 5 to 7 years", {group:"Children 5 to 7 years",cu:0}],
+  ["Children 5 to 7 years", {group:"Children 5 to 7 years",cu:0.6}],
   ["Children 3 to 5 years", {group:"Children 3 to 5 years",cu:0.5}],
   ["Children 1 to 3 years", {group:"Children 1 to 3 years",cu:0.4}],
 ]);
