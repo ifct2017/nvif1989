091066767fd1c5e4830ecd89d634e8fc7b8a95ef
Subhajit Sahu <wolfram77@gmail.com>
2020-02-02 11:45:49 +0530

    corpus generated

---
+++ corpus.js
@@ -0,0 +1,9 @@
+const CORPUS = new Map([
+  ["Sedentary, Male", {activity:"Sedentary, Male",occupation:"Teacher, Tailor, Barber, Executives, Shoe-maker, Priest, Retired Personnel, Land-Lord,Peon, Postman, etc."}],
+  ["Sedentary, Female", {activity:"Sedentary, Female",occupation:"Teacher, Tailor, Executives, Housewife, Nurses, etc."}],
+  ["Moderate, Male", {activity:"Moderate, Male",occupation:"Fisherman, Basket-maker, Potter, Goldsmith, Agricultural labour, Carpenter, Mason, Rick-shaw-puller, Electrician, Fitter, Turner, Welder, Industrial labourer, Cooli, Weaver, Driver, etc."}],
+  ["Moderate, Female", {activity:"Moderate, Female",occupation:"Servant-maid, Cooli, Basket-maker, Weaver, Agricultural labourer, Beedi-maker, etc."}],
+  ["Heavy, Male", {activity:"Heavy, Male",occupation:"Stone-cutter, Blacksmith, Mine-worker, Wood-cutter, Gang-man, etc."}],
+  ["Heavy, Female", {activity:"Heavy, Female",occupation:"Stone-cutter"}],
+]);
+module.exports = CORPUS;
